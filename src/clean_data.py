9c9bf4b4da072cb8eb262d611845305bdc3272ed
Aanchal Mittal <aanchal.mittal@magway.com>
2022-11-22 20:26:24 +0000

    tests against clean.data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,21 +1,21 @@
 import pandas as pd
 from scipy import signal
 
-def standardise_data_cols(df, column_to_standardise):
-    df[column_to_standardise] = df[column_to_standardise] - df.at[0, column_to_standardise]
+def standardise_data_col(df, col_to_standardise):
+    if col_to_standardise in df.columns:
+        df[col_to_standardise] = df[col_to_standardise] - df.at[0, col_to_standardise]
     return df
+
 
 def normalise_data_col(df, col):
-    df[col] = (df[col] - df[col].mean())
-    return df
-
-def filter_data_by_col_val(df, col, col_val):
-    df = df[df[col] == col_val].reset_index(drop = 'True')
+    if col in df.columns:
+        df[col] = (df[col] - df[col].mean())
     return df
 
 
 def filter_data_by_col_vals(df, col, col_vals_list):
-    df = df[df[col].isin(col_vals_list)]
+    if col in df.columns:
+        df = df[df[col].isin(col_vals_list)].reset_index(drop = "True")
     return df
 
 
@@ -27,7 +27,6 @@ def select_data_columns(df, col_list):
 def start_data_after_first_peak(df, prominence, col):
     peaks = signal.find_peaks((df[col]).to_numpy(), prominence = prominence)
     first_peak_index = peaks[0][0]
-    
     cropped_df = df[first_peak_index:df.index[-1]]
     cropped_df = cropped_df.reset_index(drop = True)
 
@@ -35,5 +34,7 @@ def start_data_after_first_peak(df, prominence, col):
 
 
 def get_data_with_col_greater_than_val(df, val, col):
-    df = df[df[col] > val ]
+    if col in df.columns:
+        df = df[df[col] > val].reset_index(drop = True)
+    
     return df
---
+++ tests/test_clean.py
@@ -0,0 +1,138 @@
+import unittest
+import sys
+import os.path
+from pathlib import Path
+import pandas as pd
+
+sys.path.append(rf"{Path(__file__).parent.parent}\src")
+import clean_data as cl 
+
+class test_standardise_data_col(unittest.TestCase):
+
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_normalised(self):
+        actual_df = cl.standardise_data_cols(self.df, "B").to_string()
+        expected_df = pd.DataFrame({'A': [1, 2, 3], 'B': [0, 2, 4], 'C': [9, 12, 15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_same_df(self):
+        actual_df = cl.standardise_data_cols(self.df, "D").to_string()
+        expected_df = (self.df).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_not_integers_returns_same_df(self):
+        return "need to implement"
+
+
+class test_standardise_data_col(unittest.TestCase):
+
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_normalised(self):
+        actual_df = cl.standardise_data_col(self.df, "B").to_string()
+        expected_df = pd.DataFrame({'A': [1, 2, 3], 'B': [0, 2, 4], 'C': [9, 12, 15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_same_df(self):
+        actual_df = cl.standardise_data_col(self.df, "D").to_string()
+        expected_df = (self.df).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_not_integers_returns_same_df(self):
+        return "need to implement"
+    
+
+class test_normalise_data_col(unittest.TestCase):
+
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_normalised(self):
+        actual_df = cl.normalise_data_col(self.df, "B").to_string()
+        expected_df = pd.DataFrame({'A': [1, 2, 3], 'B': [-2.0, 0.0, 2.0], 'C': [9, 12, 15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_same_df(self):
+        actual_df = cl.normalise_data_col(self.df, "D").to_string()
+        expected_df = (self.df).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_not_integers_returns_same_df(self):
+        return "need to implement"
+
+
+class test_filter_data_by_col_vals(unittest.TestCase):
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_correct_df(self):
+        actual_df = cl.filter_data_by_col_vals(self.df, "A", [2, 3]).to_string()
+        expected_df = pd.DataFrame({'A': [2, 3], 'B': [6, 8], 'C': [12, 15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_same_df(self):
+        actual_df = cl.filter_data_by_col_vals(self.df, "D", [3]).to_string()
+        expected_df = (self.df).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_no_val_returns_empty_df(self):
+        actual_df = cl.filter_data_by_col_vals(self.df, "A", [6]).to_string()
+        expected_df = pd.DataFrame({'A': [], 'B': [], 'C': []}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+class test_select_data_columns(unittest.TestCase):
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_correct_df(self):
+        actual_df = cl.select_data_columns(self.df, ["A", "C"]).to_string()
+        expected_df = pd.DataFrame({'A': [1, 2, 3], 'C': [9, 12, 15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_correct_df(self):
+        actual_df = cl.select_data_columns(self.df, ["A", "D"]).to_string()
+        expected_df = pd.DataFrame({'A': [1, 2, 3]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+class test_get_data_with_col_greater_than_val(unittest.TestCase):
+    def setUp(self) -> None:
+       self.df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 6, 8], 'C': [9, 12, 15]})
+    
+
+    def test_good_input_returns_normalised(self):
+        actual_df = cl.get_data_with_col_greater_than_val(self.df, col="B", val=6).to_string()
+        expected_df = pd.DataFrame({'A': [3], 'B': [8], 'C': [15]}).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_bad_col_returns_same_df(self):
+        actual_df = cl.get_data_with_col_greater_than_val(self.df, col="D", val=5).to_string()
+        expected_df = (self.df).to_string()
+        self.assertEqual(actual_df, expected_df, "dataframes not equal")
+
+
+    def test_not_integers_returns_same_df(self):
+        return "need to implement"
+
+
+if __name__ == '__main__':
+    unittest.main()
